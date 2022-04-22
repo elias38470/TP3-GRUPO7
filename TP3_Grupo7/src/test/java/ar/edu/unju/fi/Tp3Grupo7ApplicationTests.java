882fdacaebddb56ed3cfecb7b7a95460114db370
ELIAs <ELIAs@DESKTOP-7DD2KDS>
2022-04-22 16:08:25 -0300

    Primer Commit

---
+++ TP3_Grupo7/src/test/java/ar/edu/unju/fi/Tp3Grupo7ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp3Grupo7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
